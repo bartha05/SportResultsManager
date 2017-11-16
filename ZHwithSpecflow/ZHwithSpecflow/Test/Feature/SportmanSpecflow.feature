c1e7b86dbf2ec461732f0f3e1d96d3104364908f
ferenczit <ferenczi.timii@gmail.com>
2017-11-16 14:46:12 +0100

    Add Specflow test for Sportman solution

---
+++ ZHwithSpecflow/ZHwithSpecflow/Test/Feature/SportmanSpecflow.feature
@@ -0,0 +1,62 @@
+﻿Feature: SportmanSpecflow
+
+Background:
+	Given the WorldRecord is zero
+
+@myScenario
+Scenario Outline: Verify the personal best result
+	Given a sportman with name <Name>
+	And he has the highest personal results 
+	When I compare the results to the personal best
+	Then the personal best is the highest 
+Examples: 
+| Name |
+| Teofil |
+
+Scenario Outline: Verify the 'class' category
+	Given a sportman with name <Name1>
+	And he has the highest personal results
+	And another sportman with name <Name2>
+	And he has personal results
+	When I compare the results of the sportmen
+	Then the world recorder is <Name1>
+	And the category of <Name1> is class
+Examples: 
+| Name1  | Name2   |
+| Teofil | Salamon |
+
+Scenario Outline: Verify the 'professional' category
+	Given a sportman with name <Name1>
+	And he has the highest personal results
+	And another sportman with name <Name2>
+	And he has personal results, which are between the 30% of the highest and the 75% of the highest
+	When I compare the results of the sportmen
+	Then the category of <Name2> is professional
+Examples:
+| Name1  | Name2   | 
+| Teofil | Salamon | 
+
+Scenario Outline: Verify the 'amateur' category
+	Given a sportman with name <Name1>
+	And he has the highest personal results
+	And another sportman with name <Name2>
+	And he has personal results, which are lesser than the 30% of the highest
+	When I compare the results of the sportmen
+	Then the world recorder is <Name1>
+	And the category of <Name2> is amateur
+Examples: 
+| Name1  | Name2   |
+| Teofil | Salamon |
+
+Scenario Outline: Verify that there can be only one Wordlrecorder, but there can be more class sportman 
+	Given a sportman with name <Name1>
+	And he has the highest personal results
+	And another sportman with name <Name2>
+	And he has personal results which are higher than the 75% of the highest 
+	When I compare the results of the sportmen
+	Then the world recorder is <Name1>
+	And the category of <Name1> is class
+	And the category of <Name2> is also class
+Examples: 
+| Name1  | Name2   |
+| Teofil | Salamon | 
---
+++ ZHwithSpecflow/ZHwithSpecflow/Test/Steps/StepDefinition1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using ZH;
+
+
+namespace ZHwithSpecflow.Test.Steps
+{
+    [Binding]
+    public sealed class StepDefinition1
+    {
+        public Sportman Sportman;
+        public Sportman SportmanWithClassCategory;
+        public Extensions Extensions = new Extensions();
+
+        [Given(@"the WorldRecord is zero")]
+        public void GivenTheWorldRecordIsZero()
+        {
+            Sportman.WorldRecord = 0;
+        }
+
+        [Given(@"a sportman with name (.*)")]
+        public void GivenASportmanWithName(string name)
+        {
+            SportmanWithClassCategory = new Sportman(name);
+
+            Console.WriteLine("New sportman is created! Name: " + SportmanWithClassCategory.Name);
+        }
+
+        [Given(@"he has personal results")]
+        public void GivenHeHasPersonalResults()
+        {
+            Sportman = Extensions.CreateTheHighestResults(Sportman);
+        }
+
+        [When(@"I compare the results to the personal best")]
+        public void WhenICompareTheResultsToThePersonalBest()
+        {
+            Console.WriteLine("The world record: " + Sportman.WorldRecord);
+        }
+
+        [Then(@"the personal best is the highest")]
+        public void ThenThePersonalBestIsTheHighest()
+        {
+            Console.WriteLine("results:" + Sportman.WorldRecord + " - " + SportmanWithClassCategory.BestPersonalResult);
+            Assert.AreEqual(SportmanWithClassCategory.BestPersonalResult, Sportman.WorldRecord);
+
+        }
+
+        [Given(@"another sportman with name (.*)")]
+        public void GivenAnotherSportmanWithName(string name)
+        {
+            this.Sportman = new Sportman(name);
+            Console.WriteLine("New sportman is created! Name: " + Sportman.Name);
+        }
+
+        [Given(@"he has the highest personal results")]
+        public void GivenHeHasHighestPersonalResults()
+        {
+            SportmanWithClassCategory = Extensions.CreateTheHighestResults(SportmanWithClassCategory);
+        }
+
+        [When(@"I compare the results of the sportmen")]
+        public void WhenICompareTheResultsOfTheSportmen()
+        {
+            Console.WriteLine("World record: " + Sportman.WorldRecord);
+            Console.WriteLine(Sportman.Name + ":" + Sportman.BestPersonalResult);
+            Console.WriteLine(SportmanWithClassCategory.Name + ":" + SportmanWithClassCategory.BestPersonalResult);
+        }
+
+        [Then(@"the world recorder is (.*)")]
+        public void ThenTheWorldRecorderIs(string name)
+        {
+            Assert.AreEqual(SportmanWithClassCategory.Name, name);
+        }
+
+        [Then(@"the category of (.*) is class")]
+        public void ThenTheCategoryIsClass(string name)
+        {
+            Assert.AreEqual(SportmanWithClassCategory.PersonalCategory, "class");
+        }
+
+        [Then(@"the category of (.*) is professional")]
+        public void ThenTheCategoryfIsProfessional(string name)
+        {
+            Assert.AreEqual(Sportman.PersonalCategory, "professional");
+        }
+
+        [Then(@"the category of (.*) is amateur")]
+        public void ThenTheCategoryIsAmateur(string name)
+        {
+            Assert.AreEqual(Sportman.PersonalCategory, "amateur");
+        }
+
+        [Then(@"the category of (.*) is also class")]
+        public void ThenTheCategoryOfIsAlsoClass(string p0)
+        {
+            Assert.AreEqual(Sportman.PersonalCategory, "class");
+        }
+
+        [Given(@"he has personal results, which are between the (.*)% of the highest and the (.*)% of the highest")]
+        public void GivenHeHasPersonalResultsWhichAreBetweenTheOfTheHighestAndTheOfTheHighest(int p0, int p1)
+        {
+            Sportman = Extensions.CreateProfessionalResults(Sportman);
+        }
+
+        [Given(@"he has personal results, which are lesser than the (.*)% of the highest")]
+        public void GivenHeHasPersonalResultsWhichAreLesserThanTheOfTheHighest(int p0)
+        {
+            Sportman = Extensions.CreateAmateurResults(Sportman);
+        }
+
+        [Given(@"he has personal results which are higher than the (.*)% of the highest")]
+        public void GivenHeHasPersonalResultsWhichAreHigherThanTheOfTheHighest(int p0)
+        {
+            Extensions.CreateClassResults(Sportman);
+        }
+
+    }
+}
